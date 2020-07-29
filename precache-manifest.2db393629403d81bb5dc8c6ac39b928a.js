27fc052d8934b3182f4bf63b34f5eb40d9ee484f
waichung <weling@ucsd.edu>
2020-07-29 18:01:25 +0800

    Updates

---
+++ precache-manifest.2db393629403d81bb5dc8c6ac39b928a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49e1df580be28180f2359df379a04d10",
+    "url": "/REACT/index.html"
+  },
+  {
+    "revision": "1546ee8538ae23f18b3b",
+    "url": "/REACT/static/css/2.4c4477be.chunk.css"
+  },
+  {
+    "revision": "07cd62f980202ea4c5cc",
+    "url": "/REACT/static/css/main.06c141f6.chunk.css"
+  },
+  {
+    "revision": "1546ee8538ae23f18b3b",
+    "url": "/REACT/static/js/2.a0df81d4.chunk.js"
+  },
+  {
+    "revision": "b815bed3bedb5a8af7470bb094451139",
+    "url": "/REACT/static/js/2.a0df81d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07cd62f980202ea4c5cc",
+    "url": "/REACT/static/js/main.2c40ef8b.chunk.js"
+  },
+  {
+    "revision": "e23f6b8947e13c51e44d",
+    "url": "/REACT/static/js/runtime-main.a0cfe7b5.js"
+  }
+]);
